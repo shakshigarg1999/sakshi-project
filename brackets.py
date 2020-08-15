194224e8a39e4fef9e686142f0f27564493395d4
shakshigarg1999 <62202154+shakshigarg1999@users.noreply.github.com>
2020-08-15 17:27:01 +0530

    Update brackets.py

--- brackets.py
+++ brackets.py
@@ -1,26 +1,17 @@
 # python3
 
-#from collections import namedtuple
-
-#Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-#def are_matching(left, right):
-    #return (left + right) in ["()", "[]", "{}"]
-
-
 def find_mismatch(text):
     
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            # Process opening bracket
             opening_brackets_stack.append(next)
             if(i==len(text)-1):
                 return i+1
 
         elif next in ")]}":
-            # Process closing bracket, write your code here
+            # Process closing bracket
             if(len(opening_brackets_stack)==0):
                 return i+1
             else:
@@ -32,11 +23,9 @@ def find_mismatch(text):
         return (text.index(opening_brackets_stack[0])+1)
     return "Success"                
                     
-
-#def main():
 text = input()
 mismatch = find_mismatch(text)
-# Printing answer, write your code here
+# Printing answer
 print(mismatch)
 
 
